9b50ae79c655392611c9efca029195b8d53a0f81
Arthur Asylgareev <arthur@asylgareev>
2020-04-05 11:54:02 +0500

    Generic test was added

---
+++ YASDM.Model.Tests/GenericTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace YASDM.Model.Tests
+{
+    public class GenericTest
+    {
+        [Fact]
+        public void ShouldPass()
+        {
+            Assert.True(true);
+        }
+    }
+}
