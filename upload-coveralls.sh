b088520036f5024f9489063c874df08ef2119f12
Arthur Asylgareev <arthur@asylgareev>
2020-04-05 15:16:13 +0500

    Experimenting with current folder

--- upload-coveralls.sh
+++ upload-coveralls.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+pwd
+ls -lah
+ls YASDM.Model.Tests/TestResults/
 dotnet tool install coveralls.net --version 1.0.0 --tool-path tools        
 "./tools/csmacnz.Coveralls --opencover -i YASDM.Model.Tests/TestResults/coverage.opencover.xml --repoTokenVariable COVERALL_REPO_TOKEN --commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor "$REPO_COMMIT_AUTHOR" --commitEmail "$REPO_COMMIT_AUTHOR_EMAIL" --commitMessage "$REPO_COMMIT_MESSAGE" --jobId $TRAVIS_JOB_ID  --serviceName \"travis-ci\"" 
