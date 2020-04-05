ddb8e0f7d6c84fa41c8e923f2c74c302b003c5ab
Arthur Asylgareev <arthur@asylgareev>
2020-04-05 15:28:03 +0500

    Coveralls command invocation was fixed

--- upload-coveralls.sh
+++ upload-coveralls.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-ls YASDM.Model.Tests/TestResults/
 dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
-ls
-ls ./tools/ 
-"./tools/csmacnz.Coveralls --opencover -i ./YASDM.Model.Tests/TestResults/coverage.opencover.xml --repoTokenVariable COVERALL_REPO_TOKEN --commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor "$REPO_COMMIT_AUTHOR" --commitEmail "$REPO_COMMIT_AUTHOR_EMAIL" --commitMessage "$REPO_COMMIT_MESSAGE" --jobId $TRAVIS_JOB_ID  --serviceName \"travis-ci\"" 
+./tools/csmacnz.Coveralls --opencover -i ./YASDM.Model.Tests/TestResults/coverage.opencover.xml --repoTokenVariable COVERALL_REPO_TOKEN --commitId $TRAVIS_COMMIT --commitBranch $TRAVIS_BRANCH --commitAuthor "$REPO_COMMIT_AUTHOR" --commitEmail "$REPO_COMMIT_AUTHOR_EMAIL" --commitMessage "$REPO_COMMIT_MESSAGE" --jobId $TRAVIS_JOB_ID  --serviceName "travis-ci"
