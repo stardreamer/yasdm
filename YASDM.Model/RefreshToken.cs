793af06ce5cfc0f2a7cce1f563cc3dcb8ce2bce0
Arthur Asylgareev <arthur@asylgareev>
2020-04-07 20:25:34 +0500

    Refresh token model was added

---
+++ YASDM.Model/RefreshToken.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace YASDM.Model
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+    }
+}
