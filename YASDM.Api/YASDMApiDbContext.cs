ed2f27779093b404b65fd3e1511eecab0b28aa6b
Arthur Asylgareev <arthur@asylgareev>
2020-04-07 20:26:56 +0500

    Refreshtokens were added to EF dbcontext

--- YASDM.Api/YASDMApiDbContext.cs
+++ YASDM.Api/YASDMApiDbContext.cs
@@ -16,6 +16,8 @@ namespace YASDM.Api
 
         public DbSet<UserRoom> UserRooms { get; set; }
 
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -24,6 +26,8 @@ namespace YASDM.Api
             userEntity.HasIndex(u => u.UserName).IsUnique();
 
             userEntity.HasKey(user => user.Id);
+
+            userEntity.HasMany(u => u.UserRooms).WithOne(rt => rt.User).HasForeignKey(rt => rt.UserId).OnDelete(DeleteBehavior.Cascade);
 
             var roomEntity = modelBuilder.Entity<Room>();
 
@@ -36,6 +40,9 @@ namespace YASDM.Api
             userRoomEntity.HasOne(ur => ur.Room).WithMany(r => r.UserRooms).HasForeignKey(ur => ur.RoomId).OnDelete(DeleteBehavior.Cascade);
 
             userRoomEntity.HasIndex(c => new { c.UserId, c.RoomId }).IsUnique();
+
+            var refreshTokenEntity = modelBuilder.Entity<RefreshToken>();
+            refreshTokenEntity.HasIndex(rt => rt.Token).IsUnique();
         }
     }
 }
