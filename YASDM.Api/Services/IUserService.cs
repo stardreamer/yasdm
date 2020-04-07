33cba84e98edf94695ec2ff48943bc1740c72652
Arthur Asylgareev <arthur@asylgareev>
2020-04-07 18:27:41 +0500

    UserService interface was added

---
+++ YASDM.Api/Services/IUserService.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YASDM.Model;
+using YASDM.Model.DTO;
+
+namespace YASDM.Api.Services
+{
+    public interface IUserService
+    {
+        Task<User> Authenticate(string username, string password);
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetById(int id);
+
+        Task<User> GetEagerById(int id);
+
+        Task<User> Create(AuthRegisterDTO registerDTO);
+        Task UpdateCredentials(int id, AuthRegisterDTO updateDTO);
+
+        Task UpdateUser(int id, UserDTO updateDTO);
+        Task Delete(int id);
+    }
+}
