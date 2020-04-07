11a0377886685237ec597f7332479dc23645e94d
Arthur Asylgareev <arthur@asylgareev>
2020-04-07 18:27:23 +0500

    Necessary migrations were added

--- YASDM.Api/Migrations/20200406131717_PasswordsAdded.Designer.cs
+++ YASDM.Api/Migrations/20200406131717_PasswordsAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using YASDM.Api;
@@ -9,9 +10,10 @@ using YASDM.Api;
 namespace YASDM.Api.Migrations
 {
     [DbContext(typeof(YASDMApiDbContext))]
-    partial class YASDMApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200406131717_PasswordsAdded")]
+    partial class PasswordsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ YASDM.Api/Migrations/20200406131717_PasswordsAdded.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YASDM.Api.Migrations
+{
+    public partial class PasswordsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+        }
+    }
+}
