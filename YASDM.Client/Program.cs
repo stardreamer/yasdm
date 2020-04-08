0e3d870f18b0d95f40aba6d426346835f13e364e
Arthur Asylgareev <arthur@asylgareev>
2020-04-08 20:13:41 +0500

    Necessary services were added

--- YASDM.Client/Program.cs
+++ YASDM.Client/Program.cs
@@ -4,6 +4,10 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using YASDM.Client.Services;
+using System.Net.Http;
 
 namespace YASDM.Client
 {
@@ -15,6 +19,11 @@ namespace YASDM.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddOptions();
+            builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
 
             await builder.Build().RunAsync();
         }
