84dd49b61d96f826302b5c8b2cc8b13b71ae8445
Arthur Asylgareev <arthur@asylgareev>
2020-04-09 18:39:21 +0500

    Auth DTO now has an email field

--- YASDM.Model/DTO/AuthDto.cs
+++ YASDM.Model/DTO/AuthDto.cs
@@ -5,6 +5,8 @@ namespace YASDM.Model.DTO
         public int Id { get; set; }
         public string Username { get; set; }
 
+        public string Email { get; set; }
+
         public string Token { get; set; }
 
         public string RefreshToken { get; set; }
