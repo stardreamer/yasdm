f13db7c4653e65f3da936a9190b6ece7ad08ec6f
Arthur Asylgareev <arthur@asylgareev>
2020-04-15 17:15:35 +0500

    Profile page was added

---
+++ YASDM.Client/Pages/Profile.razor.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using YASDM.Client.Services;
+using YASDM.Model.DTO;
+
+namespace YASDM.Client.Pages
+{
+    public class ProfileViewModel : ComponentBase
+    {
+        
+
+    }
+}
