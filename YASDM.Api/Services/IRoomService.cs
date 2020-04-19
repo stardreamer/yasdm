0b2e300d975bb21c4e3ce34018d7f4dd4f1f9e95
Arthur Asylgareev <arthur@asylgareev>
2020-04-19 13:27:30 +0500

    IRoomService was added

---
+++ YASDM.Api/Services/IRoomService.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YASDM.Model;
+using YASDM.Model.DTO;
+
+namespace YASDM.Api.Services
+{
+    public interface IRoomService
+    {
+        Task<IEnumerable<Room>> GetAll();
+
+        Task<IEnumerable<Room>> GetPaginated(PaginationDTO paginationParameters);
+        Task<Room> GetById(int id);
+
+        Task<Room> GetEagerById(int id);
+
+        Task<Room> Create(RoomDTO roomDTO);
+
+        Task Update(int id, RoomDTO roomDTO);
+
+        Task Delete(int id);
+    }
+}
