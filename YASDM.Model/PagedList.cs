a66a877298040a4fb630885fd0c13b4f88e32bb9
Arthur Asylgareev <arthur@asylgareev>
2020-04-26 13:08:01 +0500

    PagedList was moved to model library

--- YASDM.Model/PagedList.cs
+++ YASDM.Model/PagedList.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
-namespace YASDM.Api
+namespace YASDM.Model
 {
     public class PagedList<T> : List<T>
     {
@@ -25,21 +21,6 @@ namespace YASDM.Api
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
             AddRange(items);
-        }
-    }
-
-    public static class PagedListExtensions
-    {
-        public static async Task<PagedList<T>> ToPagedListAsync<T, TKey>(this IQueryable<T> superset, Expression<Func<T, TKey>> keySelector, int pageNumber, int pageSize)
-        {
-            var count = await superset.CountAsync();
-            var items = await superset.OrderBy(keySelector)
-            .Skip((pageNumber - 1) * pageSize)
-            .Take(pageSize)
-            .ToListAsync();
-
-            return new PagedList<T>(items, count, pageNumber, pageSize);
-
         }
     }
 }
