26b79dffc0b59eb39a9a7515e32a9e7b2484c33f
Arthur Asylgareev <arthur@asylgareev>
2020-05-11 10:59:06 +0500

    Necessary migration were added

---
+++ YASDM.Api/Migrations/20200511055536_UserPairsAdded.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace YASDM.Api.Migrations
+{
+    public partial class UserPairsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserPairs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoomId = table.Column<int>(nullable: false),
+                    User1Id = table.Column<int>(nullable: false),
+                    User2Id = table.Column<int>(nullable: false),
+                    User1Alias = table.Column<string>(nullable: true),
+                    User2Alias = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPairs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserPairs_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPairs_RoomId_User1Id",
+                table: "UserPairs",
+                columns: new[] { "RoomId", "User1Id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPairs_RoomId_User2Id",
+                table: "UserPairs",
+                columns: new[] { "RoomId", "User2Id" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPairs");
+        }
+    }
+}
--- YASDM.Api/YASDMApiDbContext.cs
+++ YASDM.Api/YASDMApiDbContext.cs
@@ -16,6 +16,8 @@ namespace YASDM.Api
 
         public DbSet<UserRoom> UserRooms { get; set; }
 
+        public DbSet<UserPair> UserPairs { get; set; }
+
         public DbSet<RefreshToken> RefreshTokens { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,7 +26,7 @@ namespace YASDM.Api
 
             var userEntity = modelBuilder.Entity<User>();
             userEntity.HasIndex(u => u.UserName).IsUnique();
-            userEntity.HasIndex(u=> u.Email).IsUnique();
+            userEntity.HasIndex(u => u.Email).IsUnique();
 
             userEntity.HasKey(user => user.Id);
 
@@ -33,6 +35,8 @@ namespace YASDM.Api
             var roomEntity = modelBuilder.Entity<Room>();
 
             roomEntity.HasKey(room => room.Id);
+
+            roomEntity.HasMany(r => r.UserPairs).WithOne(up => up.Room).HasForeignKey(up => up.RoomId).OnDelete(DeleteBehavior.Cascade);
 
 
             var userRoomEntity = modelBuilder.Entity<UserRoom>();
@@ -44,6 +48,12 @@ namespace YASDM.Api
 
             var refreshTokenEntity = modelBuilder.Entity<RefreshToken>();
             refreshTokenEntity.HasIndex(rt => rt.Token).IsUnique();
+
+            var userPairsEntity = modelBuilder.Entity<UserPair>();
+
+            userPairsEntity.HasKey(up => up.Id);
+            userPairsEntity.HasIndex(up => new { up.RoomId, up.User1Id }).IsUnique();
+            userPairsEntity.HasIndex(up => new { up.RoomId, up.User2Id }).IsUnique();
         }
     }
 }
